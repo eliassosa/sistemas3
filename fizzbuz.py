eff52c14c297c665e5a38aff77aaef5024f65bdd
Raul Elias Sosa <sraulelias@gmail.com>
2015-05-31 16:20:41 -0300

    agregamos cambio de codigo

--- fizzbuz.py
+++ fizzbuz.py
@@ -1,9 +1,9 @@
 for i in xrange(1, 101):
     if i % 15 == 0:
-        print "FizzBuzz"
+        print "hola"
     elif i % 3 == 0:
-        print "Fizz"
+        print "chau"
     elif i % 5 == 0:
-        print "Buzz"
+        print "hola-chau"
     else:
         print i
